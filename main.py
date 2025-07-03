175bc6706495c30b368e6f804f643c0026cd5b99
soustruh <martin@struzsky.cz>
2025-07-04 00:08:33 +0200

    running using docker + copy-pasteable user parameters file 📋

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import httpx
-from keboola.component import CommonInterface
+import logging
 
-from src import config
+import httpx
+from keboola.component import CommonInterface, UserException
+
+from src import shapes
 
 
 def get_request(url) -> httpx.Response:
@@ -10,20 +12,33 @@ def get_request(url) -> httpx.Response:
         response.raise_for_status()
         return response
     except httpx.HTTPError as e:
-        print(f"Error fetching data from {url}: {e}")
-        return None
+        raise Exception(f"Error fetching data from {url}: {e}")
 
 
 def main():
-    cfg = config.Config()
 
-    if resp := get_request(cfg.endpoint):
-        print(resp.request.url, resp.status_code)
-
+    # accessing Keboola Common Interface
     ci = CommonInterface()
-
-    print("User parameters:", ci.configuration.parameters)
+    params = ci.configuration.parameters
+    print("User parameters:", params)
     print("Data folder path:", ci.data_folder_path)
 
+    # running code from an imported module
+    a = params.get("rectangle_a")
+    b = params.get("rectangle_b")
+    if a is None or b is None:
+        raise UserException("Parameters 'rectangle_a' and 'rectangle_b' must be provided.")
+
+    r = shapes.Rectangle(a=a, b=b)
+    logging.info("Rectangle area: %s", r.area())
+    logging.info("Rectangle perimeter: %s", r.perimeter())
+
+    # making an HTTP request
+    endpoint = params.get("endpoint")
+    if not endpoint:
+        raise UserException("Parameter 'endpoint' must be provided.")
+
+    if resp := get_request(endpoint):
+        print(resp.request.url, resp.status_code)
 
 main()
--- src/config.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Config:
-    endpoint: str = "https://www.example.com"
-    password: str = "NEVER INCLUDE PASSWORDS OR OTHER SENSITIVE DATA HERE"
